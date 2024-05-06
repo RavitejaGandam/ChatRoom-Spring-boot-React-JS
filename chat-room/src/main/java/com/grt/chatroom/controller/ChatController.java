282a1f1ac3eed76ab6a0989c2d28acb922bac51a
Raviteja Gandam <gandamraviteja@gmail.com>
2024-05-06 15:58:15 +0530

    Backend Dev completed

--- chat-room/src/main/java/com/grt/chatroom/controller/ChatController.java
+++ chat-room/src/main/java/com/grt/chatroom/controller/ChatController.java
@@ -1,7 +1,28 @@
 package com.grt.chatroom.controller;
 
+import com.grt.chatroom.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController {
+
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+
+    @MessageMapping("/message")
+    @SendTo("/chatroom/public")
+    public Message receivePublicMessage(@Payload Message message){
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message){
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
+        return message;
+    }
+
 }
